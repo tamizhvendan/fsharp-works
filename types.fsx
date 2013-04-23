fdb245a7e7f296773c71b4ce04ef4665054b5f11
Tamizhvendan S <tamizh88@gmail.com>
2013-04-23 10:54:04 +0530

    Adding types.fsx

--- types.fsx
+++ types.fsx
@@ -12,8 +12,12 @@ type UsAddress = {StreetAddress:StreetAddress; Region: ZipAndState}
 
 // Union Types
 type Address = 
-    | UkAddress 
-    | UsAddress
+    | UK of UkAddress 
+    | US of UsAddress
+
+
+type Person = { Name : string;  Address : Address; }
+
 
 // Creating objects of types
 let streetAddr = { Line1 = "5-168"; Line2 = Some("Main Road"); Line3 = None }
@@ -21,7 +25,19 @@ let zipCode = ZipCode  "609107"
 let stateAbbre = StateAbbre "TN"
 
 let zipAndState = {State = stateAbbre; Zip = zipCode}
-let ukAddr : UkAddress = { StreetAddress = streetAddr; Region = zipCode }
-let usAddr : UsAddress = { StreetAddress = streetAddr; Region = zipAndState }   
+let ukAddr = UK { StreetAddress = streetAddr; Region = zipCode }
+let usAddr = US { StreetAddress = streetAddr; Region = zipAndState }   
 
 
+let bobInUK = { Name = "Bob"; Address = ukAddr } 
+let alexInUS = { Name = "Alex"; Address = usAddr } 
+
+
+let displayPerson person = 
+    printfn "Name : %s" person.Name
+    match person.Address with
+    | UK addr -> printfn "Street : %A, Region : %A" addr.StreetAddress addr.Region
+    | US addr -> printfn "Street : %A, Region : %A" addr.StreetAddress addr.Region.State
+
+displayPerson bobInUK
+displayPerson alexInUS
