42b9f191538a35a3f9dc9fd295aac22784fd17f8
Tamizhvendan S <tamizh88@gmail.com>
2013-05-20 09:15:32 +0530

    Adding functional learnings

---
+++ functions_as_interfaces.fsx
@@ -0,0 +1,19 @@
+﻿let incrementByOne number = number + 1
+
+let subtractByTwo number = number - 2
+
+
+let loggableFunction func param =
+    printfn "function invoked with param %A" param
+    let result = func param
+    printfn "%A" (func.GetType())
+    printfn "function returns the output %A" result
+    result
+
+
+loggableFunction incrementByOne 5
+
+loggableFunction subtractByTwo 3
+    
+
+
---
+++ strategy_pattern.fsx
@@ -0,0 +1,11 @@
+﻿type Animal(noiseMakingStrategy) = 
+    member this.MakeNoise = 
+        noiseMakingStrategy() |> printfn "Making noise %s"
+
+
+let dog = Animal ( fun () -> "Woof" )
+let cat = Animal ( fun () -> "Meow" )
+
+dog.MakeNoise
+
+cat.MakeNoise
