d9af7955c43ba0cfd23183e381c8f78c818b0bf5
Tamizhvendan S <tamizh88@gmail.com>
2014-09-30 16:48:11 +0530

    Reorganizing Projects

--- 1.getting_started/expressions.fsx
+++
@@ -1,6 +0,0 @@
-﻿let number = 4
-let isEven n = 
-    n % 2 = 0 // No return statement; Last line treated as expression
-
-// In F# 'if' is an expression
-let result = if isEven number then "Even" else "Odd" 
