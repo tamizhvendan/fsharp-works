08e38b596c4b72141c9ca77613e722ec794f1cac
Tamizhvendan S <tamizh88@gmail.com>
2014-10-01 14:57:04 +0530

    Adding Homework solution project for CIS194

---
+++ CIS194/Homework.Tests/CreditCardNumberValidationTests.fs
@@ -0,0 +1,34 @@
+﻿module CreditCardNumberValidationTests
+
+open CreditCardNumberValidation
+open FsUnit
+open NUnit.Framework
+
+[<Test>]
+let ``toDigits Convert positive integers to a list of digits``() =
+    toDigits 1234L |> should equal [1;2;3;4]
+
+[<Test>]
+let ``toDigits return empty array for 0``() =
+    toDigits 0L |> should equal []
+    
+[<Test>]
+let ``toDigits return empty array for negative numbers``() =
+    toDigits -1L |> should equal []
+    toDigits -100L |> should equal []
+    toDigits -23L |> should equal []
+
+[<Test>]
+let ``doubleEveryOther should double every other number``()=
+    doubleEveryOther [8;7;6;5] |> should equal [16;7;12;5]
+    doubleEveryOther [1;3;8;6] |> should equal [2;3;16;6]
+    doubleEveryOther [1;2;3] |> should equal [1;4;3]
+
+[<Test>]
+let ``sumDigits calculate sum of all the digits``()=
+    sumDigits [16L;7L;12L;5L] |> should equal 22
+
+[<Test>]
+let ``validate credit card``()=
+    validate 4012888888881881L |> should equal true
+    validate 4012888888881882L |> should equal false
---
+++ CIS194/Homework/CreditCardNumberValidation.fs
@@ -0,0 +1,30 @@
+﻿module CreditCardNumberValidation
+
+open System
+
+let toDigits number = 
+    let convertCharToNumericalValue c = Char.GetNumericValue c |> int
+    match number with
+    | x when x <= 0L -> List.empty
+    | x -> string x |> Seq.map convertCharToNumericalValue |> Seq.toList 
+
+        
+let doubleEveryOther numbers =
+
+    let rec doubleEveryOther' canDoubleHead numbers = 
+        match numbers with
+        | [] -> []
+        | [x] -> if canDoubleHead then [x*2] else [x]
+        | [x;y] -> if canDoubleHead then [x*2;y] else [x;y*2]
+        | x::y::xs  -> x::(y * 2)::doubleEveryOther' false xs
+ 
+    numbers |> List.rev |> doubleEveryOther' false |> List.rev
+
+let sumDigits numbers =
+    numbers |> List.map (fun x -> (toDigits x |> List.sum)) |> List.sum 
+
+let validate creditCardNumber =
+    let sum = toDigits >> doubleEveryOther >> List.map int64 >> sumDigits
+    (sum creditCardNumber % 10) = 0
+    
+
